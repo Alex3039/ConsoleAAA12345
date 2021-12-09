e6b447913b60887ac3d0adf7eee6573fe99da240
Lora Petrova <alex3039@gmail.com>
2021-12-09 16:10:18 +0300

    111111

--- ConsoleAAA12345/Program.cs
+++ ConsoleAAA12345/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAAA12345
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!4444");
         }
     }
 }
