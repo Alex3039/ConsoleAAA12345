8034f8f2c91b54a2d3332cbd72fe7639741a0393
Lora Petrova <alex3039@gmail.com>
2021-12-09 18:00:48 +0300

    Прога для подсчета массива

--- ConsoleAAA12345/Program.cs
+++ ConsoleAAA12345/Program.cs
@@ -6,9 +6,27 @@ namespace ConsoleAAA12345
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Class1 class1 = new();
-            class1.Display();
+            Console.Clear();
+            Console.Title = "Работа с консолью";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            // Console.WriteLine("Hello World!");
+            int kk = 0;
+            Console.Write("Введите числа: ");
+            string s = Console.ReadLine();
+            s = s.Replace(' ', ','); // add zapatua
+            string[] array = s.Split(','); // data
+
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"Массив чисел: { array[i] }");
+
+            for (int a = 0; a < array.Length; a++)
+                kk += Convert.ToInt32(array[a]);
+            Console.WriteLine($"Сумма: { kk }");
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
